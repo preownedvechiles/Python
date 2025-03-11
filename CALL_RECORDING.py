d5b6fb5bd3888316141bb24031ec0c035ce0a3ca
preownedvechiles <nrktechnicalprgmanager@gmail.com>
2025-03-11 16:08:09 +0530

    Update CALL_RECORDING.py

--- CALL_RECORDING.py
+++ CALL_RECORDING.py
@@ -1,3 +1,4 @@
+
 import os
 from google.cloud import bigquery
 import pyodbc
@@ -12,7 +13,7 @@ client = bigquery.Client()
 server = 'USPA01BISQL01'
 database = 'DWH'
 username = 'TL2020'
-password = 'TEternal2021!'  # Consider using environment variables for security
+password = 'TEternal2021!' # Consider using environment variables for security
 
 # Define the ODBC connection string
 conn_str = f"""
@@ -25,7 +26,8 @@ conn_str = f"""
 
 # Define the query to fetch data from BigQuery
 query = """
-SELECT  distinct  id,timestamp,userId,callId,type,filename,duration,`from`,`to`,deletedAt,createdAt,events,'sti' TenantId FROM `thrio-prod-sti.sti.CALL_RECORDING` 
+SELECT distinct id, timestamp, userId, callId, type, filename, duration, `from` AS from_address, `to`, deletedAt, createdAt, events, 'sti' AS TenantId 
+FROM `thrio-prod-sti.sti.CALL_RECORDING` 
 WHERE DATE(_PARTITIONTIME) > (SELECT max(DATE(_PARTITIONTIME))-1 MaxDate FROM `thrio-prod-sti.sti.CALL_RECORDING`)
 """
 
@@ -42,26 +44,28 @@ try:
     # Insert or update query using MERGE
     merge_query = """
     MERGE INTO import.CallRecording AS target
-    USING (SELECT ? AS id, ? AS timestamp,? AS userId, ? AS callId,? AS type, ? AS filename, ? AS duration, ? AS from,? AS to, ? AS deletedAt, ? AS createdAt, ? AS events,? AS TenantId
+    USING (SELECT ? AS id, ? AS timestamp,? AS userId, ? AS callId,? AS type, ? AS filename, ? AS duration, ? AS from_address,? AS to, ? AS deletedAt, ? AS createdAt, ? AS events,? AS TenantId
     )     
     AS source
     ON target.id = source.id 
     WHEN MATCHED THEN
-        UPDATE SET   timestamp = source.timestamp, userId = source.userId, callId= source.callId , 
+        UPDATE SET timestamp = source.timestamp, userId = source.userId, callId= source.callId , 
                    type = source.type, filename = source.filename, duration = source.duration,
-                   from = source.from,to = source.to, deletedAt = source.deletedAt, createdAt = source.createdAt,events = source.events,TenantId = source.TenantId
+                   from_address = source.from_address, to = source.to, deletedAt = source.deletedAt, 
+                   createdAt = source.createdAt, events = source.events, TenantId = source.TenantId
     WHEN NOT MATCHED THEN
-        INSERT (id,timestamp,userId,callId,type,filename,duration,from,to,deletedAt,createdAt,events, TenantId )
+        INSERT (id,timestamp,userId,callId,type,filename,duration,from_address,to,deletedAt,createdAt,events, TenantId )
         VALUES (source.id, source.timestamp, source.userId, 
-                source.callId,source.type,source.filename, source.duration, getattr(source, 'from'), source.to, source.deletedAt,source.createdAt,source.events,source.TenantId);
+                source.callId, source.type, source.filename, source.duration, source.from_address, source.to, source.deletedAt, 
+                source.createdAt, source.events, source.TenantId);
     """
 
     # Insert results into SQL Server
     for row in results:
         data_to_insert = (
             row.id, row.timestamp, row.userId, 
-            row.callId,row.type,row.filename, row.duration, getattr(row, 'from'), row.to, row.deletedAt,row.createdAt,row.events,row.TenantId
-                         )
+            row.callId, row.type, row.filename, row.duration, row.from_address, row.to, row.deletedAt, row.createdAt, row.events, row.TenantId
+        )
         cursor.execute(merge_query, data_to_insert)
 
     # Commit transaction
@@ -78,3 +82,4 @@ finally:
     if conn:
         conn.close()
     print("Database connection closed.")
+

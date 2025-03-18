a28bb92bbdc5dece23acce3efb005b5e407038a5
preownedvechiles <nrktechnicalprgmanager@gmail.com>
2025-03-18 23:48:59 +0530

    Update SURVEY_RESULTS.py

--- SURVEY_RESULTS.py
+++ SURVEY_RESULTS.py
@@ -34,7 +34,7 @@ database = 'DWH'
 
 username = 'TL2020'
 
-password = 'TEternal2021!'  # Consider using environment variables for security
+password = '!'  # Consider using environment variables for security
 
  
 
